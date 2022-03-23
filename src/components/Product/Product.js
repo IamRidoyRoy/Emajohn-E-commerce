4464276636bbb03fd7f3164669658e2d452aeccf
Ridoy Roy <ridoyroycse53engg@gmail.com>
2022-03-23 23:49:20 +0600

    Add info and cart functionality

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,14 +2,20 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-    const { name, img, price, stock, ratings, ratingsCount } = props.product
+    const { name, img, price, stock, ratings, seller } = props.product
     return (
-        <div>
-            <div className="product">
-                <img src={img} alt="" />
-                <p className='product-name'>{name} </p>
-                <p>Price: {price} </p>
-                <p>Rating - {ratings}.0</p>
+        <div className='product'>
+            <div >
+                <div className="product-info">
+                    <img src={img} alt="" />
+                    <p className='product-name'>{name} </p>
+                    <p>Price: ${price} </p>
+                    <p>Seller: {seller} </p>
+                    <p>Rating - {ratings}.0</p>
+                </div>
+                <button onClick={() => props.handleAddToCart(props.product)} className='btn-cart'>
+                    <p>Add to cart</p>
+                </button>
             </div>
         </div>
     );
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -3,11 +3,19 @@ import Product from '../Product/Product';
 import './Shop.css'
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
+
+
     useEffect(() => {
         fetch('products.json')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, []);
+
+    const handleAddToCart = (product) => {
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
     return (
         <div className='shop-container'>
             <div className="product-container">
@@ -17,6 +25,7 @@ const Shop = () => {
 
                             product={product}
                             key={product.id}
+                            handleAddToCart={handleAddToCart}
                         >
 
                         </Product>)
@@ -25,6 +34,7 @@ const Shop = () => {
             </div>
             <div className="cart-container">
                 <h1>Order summary</h1>
+                <p>Selected item : {cart.length}</p>
             </div>
         </div>
     );
