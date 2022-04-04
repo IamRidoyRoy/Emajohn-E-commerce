86789513d986d3fa4aa70a08245f1cd77c58145f
Ridoy Roy <ridoyroycse53engg@gmail.com>
2022-04-04 09:09:39 +0600

    Remove Loading to use Link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from '../../images/Logo.svg'
 import '../Header/Header.css'
+import { Routes, Route, Link } from "react-router-dom";
+
 const Header = () => {
     return (
         <div>
@@ -8,10 +10,10 @@ const Header = () => {
                 <img src={logo} alt="" />
 
                 <div>
-                    <a href="/shop">Shop</a>
-                    <a href="/orders">Orders</a>
-                    <a href="/inventory">Inventory</a>
-                    <a href="/about">About</a>
+                    <Link to="/shop">Shop</Link>
+                    <Link to="/orders">Orders</Link>
+                    <Link to="/inventory">Inventory</Link>
+                    <Link to="/about">About</Link>
                 </div>
             </nav>
         </div>
